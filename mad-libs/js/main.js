692ce11005a846dc79eb0b20356e3fd9aeb26851
Saki Uwagawa <uwagawas@uw.edu>
2015-11-03 13:59:31 -0800

    Update in class 11-3-2015

--- mad-libs/js/main.js
+++ mad-libs/js/main.js
@@ -1,8 +1,13 @@
 // Create application
-
+var myApp = angular.module('myApp', [])
 
 // Bind controller madCtrl
+myApp.controller('madCtrl', function($scope){
+    // Set initial values for name, age, location, and food
+    $scope.name = "Bob";
+    $scope.age = "13";
+    $scope.location = "mall";
+    $scope.food = "candy";
 
-
-	// Set initial values for name, age, location, and food
+})
 
